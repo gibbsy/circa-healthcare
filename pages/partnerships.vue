cfb6a1da0692079cdf61c38a598e6a606308262f
Andy Gillon <aawgillon@gmail.com>
2021-09-22 11:25:52 +0100

    textures team

--- pages/partnerships.vue
+++ pages/partnerships.vue
@@ -125,7 +125,6 @@ export default {
   mixins: [pageSetup, scrollAnimations],
   async asyncData() {
     const pageData = await sanityClient.fetch(query);
-    console.log(pageData);
     return {
       ...pageData,
     };
