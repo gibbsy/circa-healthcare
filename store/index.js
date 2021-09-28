8a145977bdefb4eddd216f1efda8421ee78f10ab
Andy Gillon <aawgillon@gmail.com>
2021-09-28 09:14:39 +0100

    sass division undo store changes

--- store/index.js
+++ store/index.js
@@ -5,7 +5,6 @@ export const state = () => ({
     hero: "xs:100vw sm:100vw md:100vw lg:50vw xl:50vw xxl:50vw hd:50vw",
     content: "xs:100vw sm:100vw md:100vw lg:100vw xl:100vw xxl:100vw hd:100vw",
   },
-  caseStudies: [],
 });
 
 export const mutations = {
@@ -14,8 +13,5 @@ export const mutations = {
   },
   setMobile(state, val) {
     state.isMobile = val;
-  },
-  setCaseStudies(state, payload) {
-    state.caseStudies = payload;
   },
 };
